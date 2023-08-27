8bfa6a47a6109715e3fe08389e074e859f80adc2
+ <+>
2023-08-27 09:13:29 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { BrowserRouter,Route } from "react-router-dom";
+
 function Home() {
   return (
     <div>
@@ -8,9 +10,46 @@ function Home() {
     </div>
   );
 }
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
 
 function App() {
   return <Home />;
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />,
+<BrowserRouter>
+    <Route path="/">
+      <Home />
+    </Route>
+    <Route path="/about">
+      <About />
+    </Route>
+    <Route path="/login">
+      <Login />
+    </Route>
+  </BrowserRouter>,
+ document.getElementById("root"));
